f3879b0b73a39f4eec45edfbfe0721bd2fedaaf6
Makayla Bryan <makaylabryan@gmail.com>
2023-03-10 14:15:55 -0500

    declared and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string dogName = "Thor";
+int dogAge = 10;
+char lastInitial = 'B';
+bool isWhite = true;
+double currentWeight = 20.23;
+decimal amountOfToys = 15.50m;
+
+Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old. His last name starts with {lastInitial}.");
+Console.WriteLine($"It is {isWhite} that he is white. He weighs {currentWeight} pounds and he has {amountOfToys} toys.");
+
