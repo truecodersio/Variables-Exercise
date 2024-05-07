7ccdee61864b2b9b1451683003c709f4e023ff3c
Damien Young <dyoung1006@gmail.com>
2024-05-07 15:00:34 -0700

    init commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,27 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {           
+            /*
+             * Assignment: declare and initialize variables for the following types:
+             * string
+             * int
+             * char
+             * bool
+             * double
+             * decimal 
+             */
+
+            string dogName = "Ralph";
+            int dogAge = 9;
+            char firstInital = dogName[0];
+            double foodAmount = 0.25;
+            decimal milesWalkDaily = 1.50m;
+            bool truth = false;
+
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old. He eats {foodAmount} cup of food per meal. We go for a walk of {milesWalkDaily} daily.  This above statment is all true: {truth}");
+            Console.ReadLine();
         }
     }
 }
